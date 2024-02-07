aacfd85d5c8c380a62f4a4e4438f8bcb6b503317
PiyushKumbhare07 <piyush07.k@gmail.com>
2024-02-08 01:02:51 +0530

    Working Navigation bar

--- src/component/Navigation_Options/Flight_Fare_Type.js
+++ src/component/Navigation_Options/Flight_Fare_Type.js
@@ -1,25 +1,37 @@
-import React from 'react'
-import "./Flight_Fare_Type.css"
+import React from "react";
+import "./Flight_Fare_Type.css";
 
 export default function Flight_Fare_Type() {
+  const fareTypes = [
+    { text: "Regular", value: 1 },
+    { text: "Armed Forces", value: 2 },
+    { text: "Student", value: 3 },
+  ];
+
   return (
     <div class="flight_fare_type_container">
-    <div class="text">Select a <br/> fare type:</div>
+      <div class="wrap_div">
+        <div class="text">
+          Select a <br /> fare type:
+        </div>
+        <div class="flight_fare_type_radio-buttons">
+          {fareTypes.map((fareType, idx) => (
+            <label key={idx}>
+              <input type="radio" name="fareType" value={fareType.value} />
+              {fareType.text} <br />
+              fares
+            </label>
+          ))}
+        </div>
+      </div>
 
-  <div class="flight_fare_type_radio-buttons">
-    <label><input type="radio" name="radioGroup"/> Regular <br/>fares</label>
-    <label><input type="radio" name="radioGroup"/> Armed Forces <br/>fares</label>
-    <label><input type="radio" name="radioGroup"/> Student <br/>fares</label>
-    <label><input type="radio" name="radioGroup"/> Senior Citizen <br/>fares</label>
-    <label><input type="radio" name="radioGroup"/> Doctor and Nurses <br/>fares</label>
-    <label><input type="radio" name="radioGroup"/> Double Seat <br/>fares</label>
-  </div>
-
-  <div class="flight_trending_flight">
-    <div class="text">Trending <br/>Searches:</div>
-    <button class="flight_trending_flight_button">Chennai - Hyderabad</button>
-    <button class="flight_trending_flight_button">Delhi - Dubai</button>
-  </div>
-</div>
-  )
+      <div class="flight_trending_flight">
+        <div class="text">
+          Trending <br />
+          Searches:
+        </div>
+        <button class="flight_trending_flight_button">Mumbai - Pune</button>
+      </div>
+    </div>
+  );
 }
--- src/component/Navigation_Options/Flight_Radio.js
+++ src/component/Navigation_Options/Flight_Radio.js
@@ -1,24 +1,38 @@
-import React from 'react'
-import "./Flight_Radio.css"
+import React, { useState } from "react";
+import "./Flight_Radio.css";
 
-export default function Flight_Radio() {
+export default function Flight_Radio(props) {
+  
+  const [flightRadio, setFlightRadio] = useState(1);
+  const flight_radios = [
+    { text: "One Way", value: 1 },
+    { text: "Round Trip", value: 2 },
+    { text: "Multi City", value: 3 },
+  ];
+
+  const handleOptionChange = (value) => {
+    setFlightRadio(value);
+    props.getRadioValue(value);
+  };
+
   return (
     <div class="flight_type_container">
-    <div class="flight_type_radio-buttons">
-      <label>
-        <input type="radio" name="options" value="1"/> One Way
-      </label>
-      <label>
-        <input type="radio" name="options" value="2"/> Round Trip
-      </label>
-      <label>
-        <input type="radio" name="options" value="3"/> Multi City
-      </label>
-    </div>
+      <div class="flight_type_radio-buttons">
+        {flight_radios.map((radio, idx) => (
+          <label key={idx}>
+            <input
+              type="radio"
+              name="options"
+              value={radio.value}
+              checked={flightRadio === radio.value}
+              onChange={() => handleOptionChange(radio.value)}
+            />
+            {radio.text}
+          </label>
+        ))}
+      </div>
 
-    <div class="text">
-      Book International and Domestic Flights
+      <div class="text">Book International and Domestic Flights</div>
     </div>
-  </div>
-  )
+  );
 }
