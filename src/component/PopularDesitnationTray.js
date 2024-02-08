0e42e59dbb95a747166929df8393711c0e6fc234
Saumitra <saumitrajagtap5@gmail.com>
2024-02-08 18:08:46 +0530

    final tray

--- src/component/PopularDesitnationTray.js
+++ src/component/PopularDesitnationTray.js
@@ -3,6 +3,7 @@ import '../component/PopularDesitnationTray.css';
 import mum from "./imagesDest/mum.jpg";
 import ba from "./imagesDest/ba.jpg";
 import pune from "./imagesDest/pune.jpg";
+import { Link } from 'react-router-dom';
 
 export default function PopularDesitnationTray() {
     const [content, setcontent] = useState([]);
@@ -10,6 +11,8 @@ export default function PopularDesitnationTray() {
         { city: "mumbai", desc: "mumbai desc", img: mum },
         { city: "bangalore", desc: "bengaluru desc", img: ba },
         { city: "pune", desc: "pune desc", img: pune },
+        { city: "re", desc: "pune desc", img: pune },
+        { city: "re", desc: "pune desc", img: pune },
         { city: "re", desc: "pune desc", img: pune },
        
     ];
@@ -22,14 +25,17 @@ export default function PopularDesitnationTray() {
         <div className="container" id="fixed">
             <div className="row" id="row2">
                 {content.map((d, index) => (
-                    <div key={index} className="col-md-3 col-sm-6 mb-3">
+                    <div key={index} className="col-md-4 col-sm-6 mb-3">
                         <div className="card" id="myCont">
                             <div className="card-body">
                                 <div id='imgheight'>
-                                <img className="card-img-top" src={d.img} alt={d.city} /></div>
-                                <div id='restdiv'><h5 className="card-title">{d.city}</h5>
-                                <p className="card-text">{d.desc}</p>
-                                <a href="#" className="btn btn-primary" id="upbutton">Book a Flight</a></div>
+                                    <img className="card-img-top" src={d.img} alt={d.city} />
+                                </div>
+                                <div id='restdiv'>
+                                    <h5 className="card-title">{d.city}</h5>
+                                    <p className="card-text">{d.desc}</p>
+                                    <Link to="/Book" className="btn btn-primary" id="upbutton">Book a Flight</Link>
+                                </div>
                             </div>
                         </div>
                     </div>
